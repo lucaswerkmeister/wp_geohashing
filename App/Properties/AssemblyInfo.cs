172119ac7f1057b3f408403cec6830de4772db0a
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-04-25 21:14:25 +0200

    Added the store description.
    Also updated the assembly info with a description.

--- App/Properties/AssemblyInfo.cs
+++ App/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Resources;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Geohashing")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Geohashing app for Windows Phone.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Geohashing")]
