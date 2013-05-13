41a88e076fac7b35e30f914ec2b3f5003addbfed
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-05-13 21:13:51 +0200

    Added german language (de-DE) support.

--- App/App.xaml.cs
+++ App/App.xaml.cs
@@ -55,7 +55,6 @@ namespace Geohashing
 				// and consume battery power when the user is not using the phone.
 				PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled;
 			}
-
 		}
 
 		// Code to execute when the application is launching (eg, from Start)
